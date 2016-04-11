b84e24e2bb037168b459496ada6da4506ba22d0b
노범영 <by@nobeom-yeongui-MacBook-Pro.local>
2016-04-11 23:20:25 +0900

    pizzastore

---
+++ src/com/j2/w6/PizzaStore.java
@@ -0,0 +1,26 @@
+package com.j2.w6;
+
+/*
+public class PizzaStore {
+  SimplePizzaFactory factory;
+  public PizzaStore(SimplePizzaFactory factory) {
+    this.factory=factory;
+  }
+  public Pizza orderPizza(String type) {
+    Pizza pizza=factory.createPizza(type);
+    Pizza.prepare();
+  }
+}
+*/
+
+public abstract class PizzaStore {
+  abstract Pizza createPizza(String type);
+  public Pizza orderPizza(String type) {
+    Pizza pizza=createPizza(type);
+    pizza.prepare();
+    pizza.bake();
+    pizza.cut();
+    return pizza;
+  }
+}
+
