b412dc39882327010b1a74fb4903be0d4ee0b0c6
노범영 <by@nobeom-yeongui-MacBook-Pro.local>
2016-04-20 17:23:34 +0900

    w7 at uni

---
+++ src/com/j2/w7/Singleton.java
@@ -0,0 +1,17 @@
+package com.j2.w7;
+
+public class Singleton {
+  // private static Singleton uniqueInstance;
+  private static Singleton uniqueInstance = new Singleton();
+  static int numCalled=0;
+  private Singleton() {
+  }
+  public static Singleton getInstance() {
+    //if(uniqueInstance == null) {
+    //  uniqueInstance = new Singleton();
+    //}
+    numCalled++;
+    System.out.println("numCalled: "+numCalled);
+    return uniqueInstance;
+  }
+}
